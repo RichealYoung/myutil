f7a1f64e94077d417689dde4b781027749f61bea
RichealYoung <609881247@qq.com>
2021-02-23 10:35:02 +0800

    fix

---
+++ loss_util.py
@@ -0,0 +1,15 @@
+import torch
+def tv_loss(img, tv_weight=5e-2):
+    """
+    Compute total variation loss.
+    Inputs:
+    - img: PyTorch Variable of shape (1, C, H, W) holding an input image.
+    - tv_weight: Scalar giving the weight w_t to use for the TV loss.
+    Returns:
+    - loss: PyTorch Variable holding a scalar giving the total variation loss
+    for img weighted by tv_weight.
+    """
+    w_variance = torch.sum(torch.pow(img[:,:,:,:-1] - img[:,:,:,1:], 2))
+    h_variance = torch.sum(torch.pow(img[:,:,:-1,:] - img[:,:,1:,:], 2))
+    loss = tv_weight * (h_variance + w_variance)
+    return loss
---
+++ transform_util.py
@@ -0,0 +1,165 @@
+import cv2
+import numpy as np
+import random
+import torch
+from einops import rearrange
+class Transform:
+    def __init__(self,operator_list,args_list):
+        self.operator_instance_list=[]
+        assert len(operator_list)==len(args_list)
+        for operator,args in zip(operator_list,args_list):
+            operator_instance=operator(**args)
+            if operator_instance.check():
+                self.operator_instance_list.append(operator_instance)
+    def __call__(self,img_list):
+        for operator_instance in self.operator_instance_list:
+            img_list = operator_instance(img_list)
+        return img_list
+class Crop:
+    def __init__(self,crop_h,crop_w,crop_h_start,crop_w_start,mod_by=0):
+        self.crop_h=crop_h
+        self.crop_w=crop_w
+        self.crop_h_start=crop_h_start
+        self.crop_w_start=crop_w_start
+        self.mod_by=mod_by
+    def check(self):
+        if (self.crop_h and self.crop_w) or self.mod_by:
+            return True
+        else:
+            print('not crop')
+            return False
+    def __call__(self,img_list,):
+        """ crop every imgs in img_list
+
+        Args:
+            img_list (list): every imgs in img_list must have shape=[H,W,C]
+
+        Returns:
+            list: a list containing all imgs which are cropped
+        """
+        img_list_cropped=[]
+        H, W, C = img_list[0].shape
+        if self.crop_h and self.crop_w:
+            crop_h = min(self.crop_h, H - self.crop_h_start)
+            crop_w = min(self.crop_w, W - self.crop_w_start)  
+            for img in img_list:
+                if self.mod_by:
+                    crop_h = (crop_h // self.mod_by) * self.mod_by
+                    crop_w = (crop_w // self.mod_by) * self.mod_by
+                img_list_cropped.append(img[self.crop_h_start:self.crop_h_start + crop_h,
+                                    self.crop_w_start:self.crop_w_start + crop_w, :])
+        elif self.mod_by:
+            for img in img_list:
+                if self.mod_by:
+                    crop_h = (H // self.mod_by) * self.mod_by
+                    crop_w = (W // self.mod_by) * self.mod_by
+                img_list_cropped.append(img[0:0 + crop_h,
+                                    0:0 + crop_w, :])
+        else:
+            img_list_cropped = img_list
+        return img_list_cropped
+class RandomCrop:
+    def __init__(self,randomcrop):
+        self.randomcrop=randomcrop
+    def check(self):
+        if self.randomcrop:
+            return True
+        else:
+            print('not randomcrop')
+            return False
+    def __call__(self,img_list,):
+        """ randomcrop every imgs in img_list
+
+        Args:
+            img_list (list): every imgs in img_list must have shape=[H,W,C]
+
+        Returns:
+            list: a list containing all imgs which are randomcropped
+        """
+        H, W, C = img_list[0].shape
+        randomcrop_h_start = random.randint(0, max(0, H - self.randomcrop))
+        randomcrop_w_start = random.randint(0, max(0, W - self.randomcrop))
+        randomcrop=Crop(self.randomcrop,self.randomcrop,randomcrop_h_start,randomcrop_w_start)
+        return randomcrop(img_list)
+class Resize:
+    def __init__(self,resize_h,resize_w):
+        self.resize_h=resize_h
+        self.resize_w=resize_w
+    def check(self):
+        if self.resize_h and self.resize_w:
+            return True
+        else:
+            print('not resize')
+            return False
+    def __call__(self,img_list,):
+        """ resize every imgs in img_list
+
+        Args:
+            img_list (list):
+
+        Returns:
+            list: a list containing all imgs which are resized
+        """
+        
+        img_list_resized = [cv2.resize(img, dsize=(self.resize_w, self.resize_h)) for img in img_list]
+        # if an img's shape=[H,W,1], then cropped img's shape will be [H,W]
+        img_list_resized = [img[...,np.newaxis] for img in img_list_resized if len(img.shape)==2]
+        return img_list_resized
+class FlipRoat:
+    def __init__(self,fliprot):
+        self.fliprot=fliprot
+    def check(self):
+        if self.fliprot:
+            return True
+        else:
+            print('not fliproat')
+            return False
+    def __call__(self,img_list,):
+        hflip = random.random() < 0.5
+        vflip = random.random() < 0.5
+        rot90 = random.random() < 0.5
+        return [fliproat_img(img, hflip,vflip,rot90) for img in img_list]
+def fliproat_img(img, hflip,vflip, rot90):
+    if hflip:
+        img = img[:, ::-1, :]
+    if vflip:
+        img = img[::-1, :, :]
+    if rot90:
+        img = img.transpose(1, 0, 2)
+    return img
+class Normalize:
+    def __init__(self,mean,std,range):
+        self.mean=mean
+        self.std=std
+        self.range=range
+    def check(self):
+        if self.mean==0 and self.std==1 and self.range==1:
+            print('not normalize')
+            return False
+        else:
+            return True
+    def __call__(self,img_list,):
+        return [(img.astype(np.float64)/self.range-self.mean)/self.std for img in img_list]
+class ToTensor:
+    def __init__(self,layout=None):
+        self.layout=layout
+    def check(self):
+        return True
+    def __call__(self,img_list,):
+        if self.layout == None:
+            return [torch.from_numpy(img).type(torch.FloatTensor) for img in img_list]
+        else:
+            return [torch.from_numpy(rearrange(img,self.layout)).type(torch.FloatTensor) for img in img_list]
+
+def crop2batch(img_list,cropsize=3):
+    imgbatch_list=[]
+    pad_size=cropsize//2
+    for img in img_list:
+        H,W,_=img.shape
+        img_pad=np.pad(img,[[pad_size,pad_size],[pad_size,pad_size],[0,0]])
+        for h in range(pad_size,H+pad_size):
+            for w in range(pad_size,W+pad_size):
+                imgcrop=img_pad[h-pad_size:h-pad_size+cropsize,w-pad_size:w-pad_size+cropsize,:]
+                imgcrop=imgcrop.flatten()
+                imgbatch_list.append(imgcrop)
+    return imgbatch_list
